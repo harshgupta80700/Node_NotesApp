9a68bb3156023ce1508283cb49fd2ba4c658e0b8
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-24 00:45:47 +0530

    importing an other js file

--- notes_app/app.js
+++ notes_app/app.js
@@ -1,5 +1,15 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('notes.txt',"This is the changed content")
+// fs.writeFileSync('notes.txt',"This is the changed content")
 
-fs.appendFileSync('notes.txt'," This is the appended content") 
+// fs.appendFileSync('notes.txt'," This is the appended content") 
+
+// fs.appendFileSync('test.txt',"This is the test content first part ")
+
+// fs.appendFileSync('test.txt',"This is the test content second part")
+
+require('./utils')
+
+const name = "Harsh Gupta"
+
+console.log(name)
