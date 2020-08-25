c5cb04c4e838cbcaa2ea9e2fc0496be261c8c2ae
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-26 00:52:13 +0530

    read data from json

--- playground/1-json.js
+++ playground/1-json.js
@@ -16,4 +16,7 @@ const bookJSON = JSON.stringify(book)
 // console.log(parsedData.title)
 // console.log(parsedData.author)
 
-fs.writeFileSync('1-json.json',bookJSON)
+//fs.writeFileSync('1-json.json',bookJSON)
+
+const DataBuffer = fs.readFileSync('1-json.json')
+console.log(DataBuffer.toString())
