44d7d811d9b23b99beb21eab42f26971fb83fe80
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-26 20:20:20 +0530

    arrow functions with objets

--- playground/2-arrow_function.js
+++ playground/2-arrow_function.js
@@ -11,3 +11,29 @@ const squareusingarrow2 = (x) => x * x
 console.log(square(3))
 console.log(squareusingarrow(5))
 console.log(squareusingarrow2(7))
+
+
+const event = {
+    name: 'Birthday party',
+    printguestList: function(){
+        console.log('This is the list of guest for the event '+ this.name)
+    }
+}
+
+// arrow functions dont bind their own object values
+
+const event2 = {
+    name: 'farewell party',
+    printguestList:() => {
+        console.log('This is the list of guest for the event '+ this.name)
+    }
+}
+
+const event3 = {
+    name: 'Convocation party',
+    printguestList: () => console.log('This is the list of guest for the event '+ this.name)
+}
+
+event.printguestList()
+event2.printguestList()
+event3.printguestList()
