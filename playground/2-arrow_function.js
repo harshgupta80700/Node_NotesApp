9a4117cb8a510d1ff443cb24027acc820c1d24ea
harshgupta80700 <harshgupta.80700@gmail.com>
2020-08-26 20:13:14 +0530

    functions with arrow notations 3rd method

--- playground/2-arrow_function.js
+++ playground/2-arrow_function.js
@@ -6,5 +6,8 @@ const squareusingarrow = (x) => {
     return x * x
 }
 
+const squareusingarrow2 = (x) => x * x
+
 console.log(square(3))
 console.log(squareusingarrow(5))
+console.log(squareusingarrow2(7))
